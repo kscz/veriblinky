6894b50c8a2a387844ebacfa7f03a4392c98cbc4
Kit Sczudlo <kit@scuzzstuff.com>
2015-06-25 23:01:34 -0700

    Massive refactoring of display_control.v to be readble and sensible

--- display_control.v
+++ display_control.v
@@ -39,6 +39,9 @@ localparam NUM_COL = (2**COL_ADDR_BITS); // Number of LEDs in a row
 localparam COLOR_DAT_WIDTH = (NUM_COL * COLOR_BITS);
 localparam ROW_DAT_WIDTH = (COLOR_DAT_WIDTH * COLOR_COUNT); // Number of bits to hold the data for 1 row
 
+localparam LAST_STATE = (NUM_COL * 2);
+localparam LAST_BCM_STATE = (2**COLOR_BITS) - 2;
+
 // Constants 
 localparam HUB_EN_ON = 1'b0;
 localparam HUB_LATCH_EN = 1'b1;
@@ -56,86 +59,31 @@ reg colorshift_en = 1'b0;
 
 // Loop variables
 reg [ROW_ADDR_BITS-1:0] cur_row = 0; // For keeping track of which row we're on
-reg [COLOR_BITS-1:0] bcm_count = (2**COLOR_BITS)-3; // Which bit are we on in the current row
-reg [COL_ADDR_BITS:0] load_shift_latch = 0; // We need (2**COL_ADDR_BITS) shift clocks and 1 more for the latch
+reg [COLOR_BITS-1:0] bcm_count = LAST_BCM_STATE - 2; // Which bit are we on in the current row
+reg [COL_ADDR_BITS+1:0] load_shift_latch = 0; // We need (2**COL_ADDR_BITS) shift clocks and 1 more for the latch
 
 // Generators!
 genvar i, j;
 integer k;
 
-generate for (i = 0; i < NUM_COL; i = i + 1) begin : PIXEL_BREAKOUT
-	localparam CUR_SRC_R = (i*COLOR_COUNT)*COLOR_BITS;
-	localparam CUR_SRC_R_END = CUR_SRC_R + COLOR_BITS - 1;
+generate for (i = 0; i < COLOR_COUNT; i = i + 1) begin : COLOR_BREAKOUT
+	for (j = 0; j < NUM_COL; j = j + 1) begin : ELEM_BREAKOUT
+		localparam CUR_SRC = ((COLOR_COUNT*j)+i)*COLOR_BITS;
+		localparam NEXT_SRC = CUR_SRC + COLOR_BITS;
 
-	localparam CUR_SRC_G = CUR_SRC_R_END + 1;
-	localparam CUR_SRC_G_END = CUR_SRC_G + COLOR_BITS - 1;
+		localparam CUR_DST = j*COLOR_BITS;
+		localparam NEXT_DST = CUR_DST + COLOR_BITS;
 
-	localparam CUR_SRC_B = CUR_SRC_G_END + 1;
-	localparam CUR_SRC_B_END = CUR_SRC_B + COLOR_BITS - 1;
-
-	localparam CUR_DST = i*COLOR_BITS;
-	localparam NEXT_DST = (i*COLOR_BITS) + COLOR_BITS - 1;
-
-	assign color_in[0][NEXT_DST:CUR_DST] = row_in[CUR_SRC_R_END:CUR_SRC_R];
-	assign color_in[1][NEXT_DST:CUR_DST] = row_in[CUR_SRC_G_END:CUR_SRC_G];
-	assign color_in[2][NEXT_DST:CUR_DST] = row_in[CUR_SRC_B_END:CUR_SRC_B];
+		assign color_in[i][NEXT_DST-1:CUR_DST] = row_in[NEXT_SRC-1:CUR_SRC];
+	end
 end
 endgenerate
-
-//generate for (i = 0; i < COLOR_COUNT; i = i + 1) begin : COLOR_BREAKOUT
-//	for (j = 0; j < NUM_COL; j = j + 1) begin : ELEM_BREAKOUT
-//		localparam CUR_SRC = ((COLOR_COUNT*j)+i)*COLOR_BITS;
-//		localparam NEXT_SRC = CUR_SRC + COLOR_BITS;
-//
-//		localparam CUR_DST = j*COLOR_BITS;
-//		localparam NEXT_DST = CUR_DST + COLOR_BITS;
-//
-//		assign color_in[i][NEXT_DST-1:CUR_DST] = row_in[NEXT_SRC-1:CUR_SRC];
-//	end
-//end
-//endgenerate
-
-//generate	for (j = 0; j < NUM_COL; j = j + 1) begin : ELEM_BREAKOUT_R
-//	localparam CUR_SRC = (COLOR_COUNT*j)*COLOR_BITS;
-//	localparam NEXT_SRC = CUR_SRC + COLOR_BITS;
-//
-//	localparam CUR_DST = j*COLOR_BITS;
-//	localparam NEXT_DST = CUR_DST + COLOR_BITS;
-//
-//	assign color_in[0][NEXT_DST-1:CUR_DST] = row_in[NEXT_SRC-1:CUR_SRC];
-//end
-//endgenerate
-
-//generate	for (j = 0; j < NUM_COL; j = j + 1) begin : ELEM_BREAKOUT_G
-//	localparam CUR_SRC = ((COLOR_COUNT*j)+1)*COLOR_BITS;
-//	localparam NEXT_SRC = CUR_SRC + COLOR_BITS;
-//
-//	localparam CUR_DST = j*COLOR_BITS;
-//	localparam NEXT_DST = CUR_DST + COLOR_BITS;
-//
-//	assign color_in[1][NEXT_DST-1:CUR_DST] = row_in[NEXT_SRC-1:CUR_SRC];
-//end
-//endgenerate
-//
-//generate	for (j = 0; j < NUM_COL; j = j + 1) begin : ELEM_BREAKOUT_B
-//	localparam CUR_SRC = ((COLOR_COUNT*j)+2)*COLOR_BITS;
-//	localparam NEXT_SRC = CUR_SRC + COLOR_BITS;
-//
-//	localparam CUR_DST = j*COLOR_BITS;
-//	localparam NEXT_DST = CUR_DST + COLOR_BITS;
-//
-//	assign color_in[2][NEXT_DST-1:CUR_DST] = row_in[NEXT_SRC-1:CUR_SRC];
-//end
-//endgenerate
-
-assign something = (outshift_en & clkdiv);
-assign something_else = (colorshift_en & clkdiv);
 
 generate for (i = 0; i < COLOR_COUNT; i= i + 1) begin : COLORSHIFTGEN
 	wire [NUM_COL-1:0] color_shift_out;	
 
 	parallel_shift #(.SHIFT_WIDTH(COLOR_BITS), .PARALLEL(NUM_COL)) color_shift (
-		.clk(clk), .en(something_else), .latch(colorshift_latch),
+		.clk(clk), .en(colorshift_en), .latch(colorshift_latch),
 		.in(color_in[i]), .out(color_shift_out)
 	);
 
@@ -143,90 +91,113 @@ generate for (i = 0; i < COLOR_COUNT; i= i + 1) begin : COLORSHIFTGEN
 		.clk(clk),
 		.in(color_shift_out), .out(s_out[i]),
 		.latch(outshift_latch),
-		.en(something)
+		.en(outshift_en)
 	);
 end
 endgenerate
 
-reg clkdiv = 0;
-
 always @(posedge clk) begin
-	clkdiv <= clkdiv + 1;
+	// State updater!
+	if (load_shift_latch == LAST_STATE) begin
+		load_shift_latch <= 0;
+	end
+	else begin
+		load_shift_latch <= load_shift_latch + 1;
+	end
 
-	if (outshift_en == SHIFT_EN_ON) begin
+	// Ouput clock handler
+	if (~load_shift_latch[0]) begin
 		hub_clk <= ~hub_clk;
 	end
 	else begin
 		hub_clk <= 1'b0;
 	end
 
-	if (clkdiv == 1'b1) begin
-		load_shift_latch <= load_shift_latch + 1;
-	end
-
-	if (load_shift_latch == 0) begin
-		if (bcm_count == 0) begin
+	// HUB75 mux pins handled here
+	if (load_shift_latch == LAST_STATE) begin
+		if (bcm_count == LAST_BCM_STATE) begin
 			hub_mux <= cur_row; // Bring the mux up to date
 		end
-		else if (bcm_count == 1) begin
-			cur_row <= cur_row + 1; // Get the RAM to fetch the next row
-		end
-
-		outshift_en <= SHIFT_EN_ON;
 	end
-	// load_shift_latch == 1 means we've clocked out 1 bit
-	else if (load_shift_latch == 1) begin
-		if (bcm_count == 1) begin
-			colorshift_en <= SHIFT_EN_ON;
-		end
 
-		if (bcm_count == 5) begin
-			colorshift_en <= SHIFT_EN_ON;
-		end
-
-		if (bcm_count == (2**COLOR_BITS)-3) begin
-			colorshift_latch <= SHIFT_LATCH_EN; // Put the 0th bit of the next row on the shifter
-		end
-
-		if (bcm_count == (2**COLOR_BITS)-2) begin
-			colorshift_en <= SHIFT_EN_ON; // Put the 1st bit of the next row on the shifter
-		end
-
-		hub_noe <= HUB_EN_ON; // Turn on the display!
-		hub_lat <= ~HUB_LATCH_EN;
-	end
-	// load_shift_latch == 2 means we've clocked out 2 bits
-	else if (load_shift_latch == 2) begin
-		colorshift_latch <= ~SHIFT_LATCH_EN;
-		colorshift_en <= ~SHIFT_EN_ON;
-	end
-	// load_shift_latch == 3 means we've clocked out 3 bits
-	// load_shift_latch == 4 means we've clocked out 4 bits
-	// ...
-	// load_shift_latch == (NUM_COL) means we've clocked out (NUM_COL - 1) bits
-	// load_shift_latch == (NUM_COL + 1) means we've clocked out (NUM_COL) bits
-	else if (load_shift_latch == NUM_COL + 1) begin
-		outshift_latch <= SHIFT_LATCH_EN; // Get the next bit latched into the outshifter
-		outshift_en <= ~SHIFT_EN_ON;
-	end
-	else if (load_shift_latch == (NUM_COL + 2)) begin
-		load_shift_latch <= 0; // Reset the clock counter
-		if (bcm_count == (2**COLOR_BITS) - 2) begin
-			bcm_count <= 0;
-			hub_noe <= ~HUB_EN_ON;
-		end
-		else begin
-			bcm_count <= bcm_count + 1; // Increment the number of rows shifted
-		end
-
-		outshift_latch <= ~SHIFT_LATCH_EN;
+	// HUB75 latch
+	if (load_shift_latch == LAST_STATE) begin
 		hub_lat <= HUB_LATCH_EN; // Latch the ouput from the previous cycle
 	end
+	else begin
+		hub_lat <= ~HUB_LATCH_EN;
+	end
+
+	// HUB75 enable
+	if (load_shift_latch == LAST_STATE - 1) begin
+		if (bcm_count == LAST_BCM_STATE) begin
+			hub_noe <= ~HUB_EN_ON;
+		end
+	end
+	else if (load_shift_latch == 2) begin
+		hub_noe <= HUB_EN_ON;
+	end
+
+	// Outshift enable pulses
+	if (~load_shift_latch[0] && load_shift_latch != (LAST_STATE-1) && load_shift_latch != LAST_STATE) begin
+		outshift_en <= SHIFT_EN_ON;
+	end
+	else begin
+		outshift_en <= ~SHIFT_EN_ON;
+	end
+	
+	// Latch the output shifter
+	if (load_shift_latch == (LAST_STATE-1)) begin
+		outshift_latch <= SHIFT_LATCH_EN; // Get the next bit latched into the outshifter
+	end
+	else begin
+		outshift_latch <= ~SHIFT_LATCH_EN;
+	end
+
+	// Colorshifter latching
+	if (load_shift_latch == 2) begin
+		if (bcm_count == LAST_BCM_STATE-1) begin
+			colorshift_latch <= SHIFT_LATCH_EN; // Put the 0th bit of the next row on the shifter
+		end
+	end
+	else begin
+		colorshift_latch <= ~SHIFT_LATCH_EN;
+	end
+
+	// Color shifter enabling
+	if (load_shift_latch == 2) begin
+		for (k = 2; ((2**k)-3) < LAST_BCM_STATE-2; k = k + 1) begin
+			if (bcm_count == ((2**k)-3)) begin
+				colorshift_en <= SHIFT_EN_ON;
+			end
+		end
+
+		if (bcm_count == LAST_BCM_STATE) begin
+			colorshift_en <= SHIFT_EN_ON; // Put the 1st bit of the next row on the shifter
+		end
+	end
+	else begin
+		colorshift_en <= ~SHIFT_EN_ON;
+	end
+
+	// Update the current row!
+	if (load_shift_latch == 0) begin
+		if (bcm_count == LAST_BCM_STATE) begin
+			cur_row <= cur_row + 1;
+		end
+	end
+
+	// BCM state update
+	if (load_shift_latch == LAST_STATE) begin
+		if (bcm_count == LAST_BCM_STATE) begin
+			bcm_count <= 0;
+		end
+		else begin
+			bcm_count <= bcm_count + 1;
+		end
+	end
 end
 
-always @(posedge clk, negedge clk) begin
-end
-
-assign next_row = cur_row;
+assign next_row = cur_row + 1;
 
 endmodule
